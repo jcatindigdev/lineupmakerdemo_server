8dd88b270ee414f37560f91bbc6441db662a646a
jesiah.catindig <jesiah.catindig@gmail.com>
2026-06-07 23:30:00 +0800

    update cors

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 5000;
 const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost:27017/pdf_builder";
 
 app.use(cors({
-  origin: "https://lineupmakerdemo-client.vercel.app/"
+  origin: "https://lineupmakerdemo-client.vercel.app"
 }));
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true, limit: "10mb" }));
